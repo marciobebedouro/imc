ee0989890f572c4e6c4524a9c4d00a7b1ef28017
MARCIO ROGERIO MACHADO <marcio.rmachado@senacsp.edu.br>
2019-09-26 22:31:39 -0300

    calculo_imc_26_09_2019

--- src/App.js
+++ src/App.js
@@ -7,151 +7,61 @@
  */
 
 import React, {useState, useEffect} from 'react';
-import {View, Text, TextInput, Alert} from 'react-native';
-
-// const calculo_imc = () => {
-
-//   const peso = parseInt(document.getElementById('peso').value);
-//   const altura = parseInt(document.getElementById)('altura').value);
-//   const resultado;
-//   resultado = peso /(Math.pow(altura,2));
-// }
-
-
+import {View, Text, TextInput, Alert, Button, StyleSheet} from 'react-native';
 
 const App = () => {
-  const [peso, setpeso] = useState('');
-  const [altura, setaltura] = useState({});
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
   const [erro, setError] = useState('');
-
-
- 
-
 
   const onchangeTextHandler = texto => {
     const regex = /^[0-9\b]+$/;
     if (regex.test(texto)) {
-      setpeso(texto);
+      setPeso(texto);
     }
   };
 
-
-
-
-  const exibirPeso = () => {
-    if (peso && peso) {
-      return (
-        <View>
-          <Text>{peso}</Text>
-        </View>
-      );
-    } else if (erro) {
-      return (
-        <View>
-          <Text>{erro}</Text>
-        </View>
-      );
+  const onchangeTextHandler2 = texto => {
+    const regex = /^[0-9\b]+$/;
+    if (regex.test(texto)) {
+      setAltura(texto);
     }
   };
 
+  const calcular = () => {
+    const resultado = parseFloat(peso) / Math.pow(parseFloat(altura), 2);
+    return Alert.alert('Calculo IMC', resultado.toString());
+  };
 
-
-
+  // const somar = (numero1, numero2) => {
+  //   const resultado = numero1 + numero2;
+  //   return resultado;
+  //   }
 
   return (
     <View>
       <Text>Digite seu peso</Text>
       <TextInput
-        value={}
+        value={peso}
         keyboardType="numeric"
-        //maxLength={8}
         onChangeText={texto => onchangeTextHandler(texto)}
       />
-      {}
+
+      <Text>Digite sua altura</Text>
+      <TextInput
+        value={altura}
+        keyboardType="numeric"
+        onChangeText={texto => onchangeTextHandler2(texto)}
+      />
+
+      <Button
+        title="Calcular"
+        onPress={() => {
+          calcular();
+        }}
+      />
     </View>
   );
 };
 
-
-
-}
-
 export default App;
-
-
-//Math.pow()
-
-// import React, {useState, useEffect} from 'react';
-// import {View, Text, TextInput, Alert} from 'react-native';
-// import axios from 'axios';
-
-// const App = () => {
-//   const [cep, setCep] = useState('');
-//   const [endereco, setEndereco] = useState({});
-//   const [erro, setError] = useState('');
-
-//   useEffect(() => {
-//     console.log('useEffect');
-//     if (cep.length === 8) {
-//       const url = `https://viacep.com.br/ws/${cep}/json/`;
-//       axios
-//         .get(url)
-//         .then(response => {
-//           if (response.data && response.status === 200) {
-//             setEndereco(response.data);
-//           }
-//         })
-//         .catch(e => {
-//           setError('Endereço não exite');
-//         });
-//     }
-//   }, [cep]);
-
-//   const onchangeTextHandler = texto => {
-//     const regex = /^[0-9\b]+$/;
-//     if (regex.test(texto)) {
-//       setCep(texto);
-//     }
-//   };
-
-//   const exibirEndereco = () => {
-//     if (endereco && endereco.logradouro) {
-//       return (
-//         <View>
-//           <Text>{endereco.logradouro}</Text>
-//         </View>
-//       );
-//     } else if (erro) {
-//       return (
-//         <View>
-//           <Text>{erro}</Text>
-//         </View>
-//       );
-//     }
-//   };
-
-//   return (
-//     <View>
-//       <Text>Digite seu CEP</Text>
-//       <TextInput
-//         value={cep}
-//         keyboardType="numeric"
-//         maxLength={8}
-//         onChangeText={texto => onchangeTextHandler(texto)}
-//       />
-//       {exibirEndereco()}
-//     </View>
-//   );
-// };
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
